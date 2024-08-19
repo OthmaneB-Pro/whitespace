53b0d07e2d7e3090a58c247fabccf082c8719aff
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-08-19 19:12:09 +0200

    ✨ HomePage : ProjectManagement > Add Page with reusable components

--- src/components/pages/homePage/projectManagement/ProjectManagement.tsx
+++ src/components/pages/homePage/projectManagement/ProjectManagement.tsx
@@ -1,12 +1,31 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import Paragraph from "../../../reusable-ui/Paragraph";
+import Button from "../../../reusable-ui/Button";
+import Image from "../../../reusable-ui/Image";
+import { FaArrowRight } from "react-icons/fa6";
 
 export default function ProjectManagement() {
   return (
-    <ProjectManagementStyled>ProjectManagement</ProjectManagementStyled>
-  )
+    <ProjectManagementStyled>
+      <div className="text">
+        <Paragraph span="Project Management" label="Images, videos, PDFs and audio files are supported. Create math expressions and diagrams directly from the app. Take photos with the mobile app and save them to a note."  />
+        <Button label="Get Started" Icon={<FaArrowRight />} />
+      </div>
+      <Image src="/img/bureau.png" alt="bureau" />
+    </ProjectManagementStyled>
+  );
 }
 
-
 const ProjectManagementStyled = styled.div`
-    height: 200vh;
-`
+  height: 160vh;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin: 140px 52px 100px 52px;
+  color: black;
+  .text{
+    display: flex;
+    height: 70vh;
+    flex-direction: column;
+    justify-content: center;
+  }
+`;
